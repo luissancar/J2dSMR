5c399973adb027ea27953a2bf2527720bd88bb50
luis jose sanchez carrasco <luissancar@yahoo.es>
2023-04-21 10:14:18 +0200

    cinco

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,40 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D player;
     private float entradaX;
 
+    public int fuerzaSalto;
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         velocidad = 10;
+        fuerzaSalto = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         entradaX = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            TocandoSuelo())
+        {
+            player.AddForce(Vector2.up * fuerzaSalto,
+                ForceMode2D.Impulse);
+        }
     }
 
     private void FixedUpdate()
     {
         player.velocity = new Vector2(entradaX * velocidad,
              player.velocity.y);
+    }
+
+    bool TocandoSuelo()
+    {
+        RaycastHit2D toca = Physics2D.Raycast(
+            transform.position + new Vector3(0,-2f, 0),
+            Vector2.down,0.2f);
+        return toca.collider != null;
     }
 }
